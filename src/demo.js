a4bd45c112ebecd93689d63815a8ea9629b68f5d
xsaxdax <60689251+xsaxdax@users.noreply.github.com>
2021-02-09 10:58:58 +0100

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { Paper } from "@material-ui/core";
+
+import SimpleModal from "./modal";
+export default function DataGridDemo() {
+  const columns = [
+    { field: "companyId", headerName: "Company ID", flex: 0.5 },
+    {
+      field: "email",
+      headerName: "E-Mail",
+      flex: 1
+    },
+    { field: "phoneNumber", headerName: "Phone Number", flex: 1 },
+    {
+      field: "contactName",
+      headerName: "Contact",
+      flex: 1
+    },
+    {
+      field: "adress",
+      renderCell: (params) => <SimpleModal row={params} />,
+      headerName: "Adress"
+    }
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      companyId: 1,
+      companyName: "x",
+      email: "a_f****_long_email@test.de",
+      phoneNumber: "055554354154",
+      contactName: "flower street",
+      adress: "gun street",
+      companyIdCRM: "jklökl",
+      manuallyProcessing: 0,
+      payforSubscribtion: 0,
+      payForCloud: 0,
+      payForNtrip: 0,
+      isAdmin: 0
+    },
+    {
+      id: 2,
+      companyId: 2,
+      companyName: "x",
+      email: "x@-.-",
+      phoneNumber: "022990787877",
+      contactName: "flower street",
+      adress: "flower street",
+      companyIdCRM: "jklökl",
+      manuallyProcessing: 0,
+      payforSubscribtion: 0,
+      payForCloud: 0,
+      payForNtrip: 0,
+      isAdmin: 0
+    }
+  ];
+
+  const onChange = (e) => <p1> {e.value} </p1>;
+
+  return (
+    <Paper>
+      <div style={{ height: 400, width: "100%" }}>
+        <div style={{ display: "flex", height: "100%" }}>
+          <div style={{ flexGrow: 1 }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              autoHeight={true}
+              showToolbar
+              hideFooter={true}
+              pagination
+              onCellHover={(e) => onChange(e)}
+              icons={<SimpleModal />}
+            ></DataGrid>
+          </div>
+        </div>
+      </div>
+    </Paper>
+  );
+}
